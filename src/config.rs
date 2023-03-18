1a28e898c54f964911388849167b1863c5b1d889
Michael Chi <chicong@outlook.com>
2023-03-19 05:06:43 +0800

    Config

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,19 @@
+use log::debug;
+use serde::Deserialize;
+use std::path::Path;
+
+#[derive(Deserialize, Debug)]
 struct Config {
-    api_port: i64,
+    log_level: String,
+    port: i64,
 }
 
-pub fn init_config() {}
+pub fn init_config() {
+    let c = config::Config::builder()
+        // .add_source(config::File::from(Path::new("config.toml")))
+        .add_source(config::File::from(Path::new("config/config.toml")))
+        .build()
+        .unwrap();
+    let conf = c.try_deserialize::<Config>().unwrap();
+    // println!("{:?}", conf);
+}
